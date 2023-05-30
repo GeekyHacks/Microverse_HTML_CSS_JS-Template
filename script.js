b633d1768ea150927a8188e2c63f4e67b5f5fade
GeekyHacks <geekyhacks22@gmail.com>
2023-05-30 17:45:32 +0300

    Edit js file

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-const btnColor = document.querySelector("test");
-const backgrod = document.getElementById("backgrod");
-
-btnColor.addEventListener("click", chBackcolor());
+const btnColor = document.querySelector('test');
+const backgrod = document.getElementById('backgrod');
 
 function chBackcolor() {
-  backgrod.style.backgroundColor = "red";
+  backgrod.style.backgroundColor = 'red';
+}
+function chcolor() {
+  backgrod.style.backgroundColor = '';
 }
 
-btnColor.addEventListener("click", chcolor());
-function chcolor() {
-  backgrod.style.backgroundColor = "";
-}
+btnColor.addEventListener('click', chBackcolor());
+
+btnColor.addEventListener('click', chcolor());
